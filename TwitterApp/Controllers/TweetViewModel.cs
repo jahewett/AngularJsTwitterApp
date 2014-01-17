2ea2344470e03f762a2a2770ab3bf567629aa27f
James Hewett <jamesahewett@gmail.com>
2014-01-17 08:01:47 +0000

    Initial check-in.

---
+++ TwitterApp/Controllers/TweetViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TwitterApp.Controllers
+{
+    public class TweetViewModel
+    {
+        public string ImageUrl { get; set; }
+        public string ScreenName { get; set; }
+        public string MediaUrl { get; set; }
+        public string Tweet { get; set; }
+        public string Id { get; set; }
+    }
+}
---
+++ TwitterApp/Scripts/hello-angular.js
@@ -0,0 +1,68 @@
+﻿var ngTwitter = angular.module("ngTwitter", ["ngResource", "ngRoute"]);
+
+ngTwitter.config(function ($routeProvider) {
+    $routeProvider.when("/", {
+        templateUrl: "timeline",
+        controller: "TimelineController"
+    });
+});
+
+ngTwitter.controller("TimelineController", function ($scope, $http, TwitterService) {
+    $scope.tweets = TwitterService.timeline.query({}, isArray = true);
+
+    $scope.sendStatus = function () {
+        var tweetText = $scope.statusText;
+
+        var newTimeLine = new TwitterService.timeline(
+        {
+            tweet: tweetText
+        });
+
+        newTimeLine.$save(function (data, headers) {
+            if (data.success && data.success == true) {
+                alert("Tweet sent successfully");
+                $scope.statusText = "";
+            }
+            else {
+                alert("Error: " + data.errorMessage);
+            }
+        });
+    }
+
+    $scope.newTweets = {
+        "0": "No new tweets",
+        "other": "{} new tweets"
+    };
+
+    $scope.retweet = function (item) {
+        var resultPromise = $http.post("/Home/Retweet/", item);
+
+        resultPromise.success(function (data) {
+            if (data.success) {
+                alert("Retweeted successfully");
+            }
+            else {
+                alert("ERROR: Retweeted failed! " + data.errorMessage);
+            }
+        });
+    }
+});
+
+ngTwitter.factory("TwitterService", function ($resource) {
+    return {
+        timeline: $resource("/Home/Tweet")
+    };
+});
+
+ngTwitter.directive("retweetButton", function () {
+    return {
+        restrict: "E",
+        transclude: true,
+        replace: true,
+        scopr: {
+            text: "@",
+            clickevent: "&"
+        },
+        template: "<button class='btn btn-xs' ng-click='clickevent()'><span class='glyphicon glyphicon-retweet'></span>{{text}}</button>"
+    }
+});
